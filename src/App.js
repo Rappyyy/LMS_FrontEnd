afb9896ad9c7a631fceb509f7fd42f5caca581ad
unknown <academy4@talentamp.com>
2023-06-28 09:30:07 +0800

    HomePubView

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import {Route, Routes} from "react-router-dom";
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import HomePubView from './components/HomePubView';
 
 
 function App() {
-  return (  
-  <Routes>
-    <Route path="/" element={< HomePubView/>} />
-    <Route path="/login" element={< LoginForm/>} />
-    
-  </Routes>
+  return (
+    <Routes>
+      <Route path="/" element={<HomePubView />} />
+      <Route path="/login" element={<LoginForm />} />
+    </Routes>
   );
 }
 
--- src/components/HomePubView.js
+++ src/components/HomePubView.js
@@ -1,3 +1,5 @@
 function HomePubView() {
   return <h1>Welcome to the Assignment Review App</h1>
 }
+
+export default HomePubView;
