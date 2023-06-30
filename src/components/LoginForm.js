9621c5eb9b055b0db2f44f0c307c5fc7a3031a13
Ralph2 <academy4@talentamp.com>
2023-06-30 09:23:39 +0800

    Login form, unauthorized. Done!

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './LoginForm.css';
 
 class LoginForm extends React.Component {
-  handleSubmit(e) {
+  handleSubmit = async (e) => {
     e.preventDefault();
 
     const username = e.target.username.value;
@@ -14,15 +17,17 @@ class LoginForm extends React.Component {
       password: password
     };
 
-    axios
-      .post('http://localhost:8080/api/auth/login', authData)
-      .then(response => {
-        const token = response.headers.authorization;
-        console.log('Token:', response.data);
-      })
-      .catch(error => {
+    try {
+      const response = await axios.post('http://localhost:8080/api/auth/login', authData);
+      const token = response.headers.authorization;
+      console.log('Token:', response.data);
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        toast.error('Unauthorized.');
+      } else {
         console.error('Login failed:', error.response);
-      });
+      }
+    }
   }
 
   render() {
