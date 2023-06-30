3ad814e15da520746ab8746776a7d5d06319dce8
Ralph2 <academy4@talentamp.com>
2023-06-30 13:39:19 +0800

    Updated Login with notification

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,11 +2,13 @@ import React from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import { useNavigate } from 'react-router-dom';
 import './LoginForm.css';
 
-class LoginForm extends React.Component {
-  handleSubmit = async (e) => {
+function LoginForm() {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const username = e.target.username.value;
@@ -21,6 +23,8 @@ class LoginForm extends React.Component {
       const response = await axios.post('http://localhost:8080/api/auth/login', authData);
       const token = response.headers.authorization;
       console.log('Token:', response.data);
+      // Redirect to authenticated page after successful login
+      navigate('/Authenticated');
     } catch (error) {
       if (error.response && error.response.status === 401) {
         toast.error('Unauthorized.');
@@ -30,24 +34,22 @@ class LoginForm extends React.Component {
     }
   }
 
-  render() {
-    return (
-      <div className="center">
-        <h1>Login</h1>
-        <form className="login-form" onSubmit={this.handleSubmit}>
-          <div className="txt_field">
-            <input type="text" name="username" required />
-            <label>Username</label>
-          </div>
-          <div className="txt_field">
-            <input type="password" name="password" required />
-            <label>Password</label>
-          </div>
-          <input className="login-button" type="submit" value="Login" />
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="center">
+      <h1>Login</h1>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <div className="txt_field">
+          <input type="text" name="username" required />
+          <label>Username</label>
+        </div>
+        <div className="txt_field">
+          <input type="password" name="password" required />
+          <label>Password</label>
+        </div>
+        <input className="login-button" type="submit" value="Login" />
+      </form>
+    </div>
+  );
 }
 
 export default LoginForm;
